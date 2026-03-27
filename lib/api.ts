383712fa7c275854381ed1664dc0b4e886465ef8
Fahim <ahmed.fahim37@gmail.com>
2026-03-27 13:05:06 +0600

    update

--- lib/api.ts
+++ lib/api.ts
@@ -165,7 +165,7 @@ export interface OpenTableItemDetailsRow {
 }
 
 export interface OpenTableDetails {
-  invoiceId?: string;
+  invoiceIds?: string[];
   tableId: string;
   tableName: string;
   waiter: string | null;
